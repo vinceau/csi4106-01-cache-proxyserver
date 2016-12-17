b8b03eb6691cda4fc6c523e04002f3a8174fb00c
Vince Au <vinceau09@gmail.com>
2016-12-17 23:24:49 +0900

    Move more header things into project_4.h

--- project_4.c
+++ project_4.c
@@ -20,76 +20,7 @@
 
 #include "time.h"
 #include "network.h"
-
-#define MAX_BUF 8192 //the max size of messages
-#define BYTESINMB 1048576 //how many bytes are in a megabyte
-
-
-struct request {
-	char method[8]; //http request method
-	char url[2048];
-	char http_v[10];
-	char host[2048];
-	char path[2048];
-	char useragent[256];
-};
-
-struct response {
-	char http_v[10];
-	int status_no;
-	char status[256];
-	char c_type[256]; //content type
-	char c_length[256]; //content length
-	int has_type;
-	int has_length;
-};
-
-struct thread_params {
-	int connfd;
-	char hoststr[NI_MAXHOST]; //readable client address
-	char portstr[NI_MAXSERV]; //readable client port
-};
-
-struct options {
-	int max_conn;
-	int max_size;
-	int comp_enabled;
-	int chunk_enabled;
-	int pc_enabled;
-};
-
-struct response_block {
-	unsigned char *response;
-	long size;
-	void *next; //NULL if complete
-};
-
-struct cache_block {
-	char host[2048];
-	char path[2048];
-	struct response_block *response;
-	int lru;
-	long size;
-	int status_no;
-	int has_type;
-	char status[256];
-	char c_type[256]; //content type
-	void *next; //NULL
-};
-
-
-int
-parse_response(char *response, struct response *r_ptr);
-
-int
-parse_request(char *request, struct request * rptr);
-
-ssize_t
-send_request(int servconn, struct request req);
-
-void
-handle_request(struct request req, struct thread_params *p);
-
+#include "project_4.h"
 
 const char *ERROR_MSG = "HTTP/1.1 403 Forbidden\r\n\r\n";
 
@@ -201,8 +132,7 @@ remove_cache(long nbytes)
 }
 
 struct cache_block*
-add_cache(char *host, char *path, char *reference, long nbytes,
-		struct response res)
+add_cache(char *host, char *path, char *reference, long nbytes, struct response res)
 {
 	if (opt.max_size > 0 && cache_size + nbytes > opt.max_size * 1000000) {
 		remove_cache(nbytes);
